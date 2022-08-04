c29249de1acd212b417c7f4466735e0890b47bd9
ramjatan32 <ramjatan1234@gmail.com>
2022-08-04 21:56:47 +0530

    initial commit

---
+++ src/main/java/mavenapp/Test.java
@@ -0,0 +1,11 @@
+package mavenapp;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to Java Programmingl");
+	}
+
+}
